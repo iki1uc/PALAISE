cd1a770431a66df6c4c9f40971070c24366d3333
iki1uc <entertrainer@web.de>
2026-07-29 08:56:38 +0200

    Update PALAISE.RESPO.js

--- PALAISE.RESPO.js
+++ PALAISE.RESPO.js
@@ -1,24 +1,24 @@
 import DB from "./DB.universal";
 import { UWM } from "./UTM.js";
 import { PALAISE_TRUTHFLOW } from "./PALAISE.TRUTHFLOW.js";
-import { TruthFlow } from "./TruthFlow.js";
+import { TruthFlow } from "./PALAISE.TRUTHFLOW.inject.js"; // dein 6D-Modul
 import { T6D } from "./TEM6D.js";
 
 export const PALAISE_RESPO = {
 
   truth(name){
 
-    // 1) Human + Universal
+    // 1) Human + Universal laden
     const human = DB.human[name];
     const universal = DB.universal[name];
 
     // 2) Analyse
     const truth = UWM.analyze(name, human, universal);
 
-    // 3) PALAISE_TRUTHFLOW.run()
+    // 3) move.edit + respo erzeugen
     const flowA = PALAISE_TRUTHFLOW.run(name);
 
-    // 4) TruthFlow.inject() (6D-Zeit)
+    // 4) 6D-Zeit TruthFlow erzeugen
     const flowB = TruthFlow.inject(
       name,
       truth,
